082a6888b824843bbe39053670439a3f7767d99f
Nancy Lee <srlee713@gmail.com>
2019-07-03 15:57:43 -0600

    Create Board class with 2 methods

---
+++ lib/board.rb
@@ -0,0 +1,28 @@
+class Board
+
+  attr_reader :board
+
+  def initialize(board={})
+    @board = board
+  end
+
+  def make_coordinates
+    cell_chars = %w[A B C D]
+    cell_nums = %w[1 2 3 4]
+
+    arr_coordinates = cell_chars.map do |char|
+      cell_nums.map do |num|
+        char + num
+      end
+    end.flatten
+  end
+
+  def cells
+    make_coordinates.each do |coord|
+      @board.store(coord, Cell.new(coord))
+    end
+
+    @board
+  end
+
+end
