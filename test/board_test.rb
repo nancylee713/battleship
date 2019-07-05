1be9cd7dcc0f13e7a7ebfe05ef36181959e819a5
Nancy Lee <srlee713@gmail.com>
2019-07-05 12:27:26 -0600

    Edit board class, add additional methods

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,44 +1,97 @@
-require 'MiniTest/autorun'
-require 'MiniTest/pride'
-require './lib/ship'
-require './lib/cell'
+require "MiniTest/autorun"
+require "MiniTest/pride"
 require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
 require 'pry'
 
 class BoardTest < MiniTest::Test
 
   def setup
-  @board = Board.new
-  @cruiser = Ship.new("Cruiser", 3)
-  @submarine = Ship.new("Submarine", 2)
-
+    @board = Board.new
+    @cell = Cell.new("A1")
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
-  # def test_it_exists
-  #
-  # assert_instance_of Board, @board
-  # end
-  #
-  # def test_it_returns_all_cell_values
-  #   skip
-  # assert_equal  UNKNOWN ,@board.cells
-  # end
-  #
-  # def test_it_cell_has_valid_coorinates
-  #
-  # assert @board.valid_coordinate?("A1")
-  # assert @board.valid_coordinate?("D4")
-  # refute @board.valid_coordinate?("A5")
-  # refute @board.valid_coordinate?("E1")
-  # refute @board.valid_coordinate?("A22")
-  #
-  # end
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
 
-  def test_valid_placement_method
+  def test_board_is_a_hash
+    assert_equal Hash, @board.cells.class
+  end
 
+  def test_board_contains_16_cells
+    assert_equal 16, @board.cells.length
+  end
 
-  #refute @board.valid_placement?(@cruiser, ["A1", "A2"])
-  #refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
-  assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+  def test_key_points_to_cell_object
+    assert_equal Cell, @board.cells["A1"].class
+  end
+
+  def test_coordinate_is_valid
+    assert @board.valid_coordinate?("A1")
+    assert @board.valid_coordinate?("D4")
+    refute @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("E1")
+    refute @board.valid_coordinate?("A22")
+  end
+
+  def test_number_of_coordinates_in_the_array_match_ship_length
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_coordinates_are_consecutive
+    refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(@submarine, ["A1", "C1"])
+    refute @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    refute @board.valid_placement?(@submarine, ["C1", "B1"])
+  end
+
+  def test_coordinates_cannot_be_diagonal
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@submarine, ["C2", "D3"])
+  end
+
+  def test_valid_placements_return_true
+    assert @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert @board.valid_placement?(@submarine, ["B2", "B3"])
+    assert @board.valid_placement?(@submarine, ["D3", "D4"])
+
+    assert @board.valid_placement?(@cruiser, ["A1", "B1", "C1"])
+    assert @board.valid_placement?(@cruiser, ["B2", "B3", "B4"])
+    assert @board.valid_placement?(@cruiser, ["B2", "C2", "D2"])
+
+    refute @board.valid_placement?(@submarine, ["E2", "F2"])
+    refute @board.valid_placement?(@submarine, ["E5", "E6"])
+    refute @board.valid_placement?(@cruiser, ["E2", "F2", "G2"])
+    refute @board.valid_placement?(@cruiser, ["E5", "E6", "E7"])
+  end
+
+  def test_board_can_place_a_ship_in_cells
+    skip
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+
+    assert_equal Cell.new("A1"), @board.cells["A1"]
+    assert_equal Cell.new("A2"), @board.cells["A2"]
+    assert_equal Cell.new("A3"), @board.cells["A3"]
+  end
+
+  def test_multiple_cells_contain_the_same_ship
+    # skip
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    cell_1 = @board.cells["A1"]
+    cell_2 = @board.cells["A2"]
+    cell_3 = @board.cells["A3"]
+
+    assert_equal @cruiser, cell_1.ship
+    assert_equal @cruiser, cell_2.ship
+    assert cell_3.ship == cell_2.ship
   end
 end
