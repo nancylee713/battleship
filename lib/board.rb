8f1110830f167ab2af5ee6332e225f564c20f81e
Nancy Lee <srlee713@gmail.com>
2019-07-06 12:15:47 -0600

    Create a render method in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -26,9 +26,9 @@ class Board
     coordinates
   end
 
-  def valid_coordinate?(coord)
-    @cells.keys.include? coord
-  end
+  # def valid_coordinate?(coord)
+  #   @cells.keys.include? coord
+  # end
 
   def valid_placement?(ship, arr_of_coords)
     # first check if arr_of_coords are valid coordinates and match ship length
@@ -36,7 +36,8 @@ class Board
     if (arr_of_coords - make_coordinates).empty? && ship.length == arr_of_coords.length
       arr_of_coords.each_cons(2).all? do |coord_1, coord_2|
         # horizontally || vertically
-        coord_1[0] == coord_2[0] && coord_2[1].to_i == coord_1[1].to_i + 1 || coord_1[1] == coord_2[1] && coord_2[0].ord == coord_1[0].ord + 1
+        coord_1[0] == coord_2[0] && coord_2[1].to_i == coord_1[1].to_i + 1 ||
+        coord_1[1] == coord_2[1] && coord_2[0].ord == coord_1[0].ord + 1
       end
     else
       false
@@ -52,6 +53,10 @@ class Board
         end
       end
     end
+  end
+
+  def render
+
   end
 
 end
