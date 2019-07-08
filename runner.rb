48e18a3d79167a9283768a603b98c7123a9a3c97
Nancy Lee <srlee713@gmail.com>
2019-07-08 16:22:36 -0600

    Start working on Turn method using a do loop
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- runner.rb
+++ runner.rb
@@ -63,28 +63,58 @@ class PlayBoardGame
       puts "Those are invalid coordinates. Please try again: \n>"
     end
 
-    # Display 2 boards
-    puts "=============COMPUTER BOARD============="
-    puts @computer_board.render
-
-    puts "==============PLAYER BOARD=============="
-    puts @user_board.render(true)
-
-    # Player turn
-    @prompts.player_turn_to_fire_on
-
     loop do
-      user_shot_input = gets.chomp()
-      valid_coordinate = @user_board.valid_coordinate? user_shot_input
-      if valid_coordinate
-        @user_board.cells[user_shot_input].fire_upon
-        puts @user_board.render(true)
+      # Display 2 boards
+      @prompts.display_computer_board
+      puts @computer_board.render
+
+      @prompts.display_user_board
+      puts @user_board.render(true)
+
+      # Player turn
+      @prompts.player_turn_to_fire_on
+
+      loop do
+        @user_shot_input = gets.chomp()
+        valid_coordinate = @user_board.valid_coordinate? @user_shot_input
+        if valid_coordinate
+          @computer_board.cells[@user_shot_input].fire_upon
+          break
+        end
+        puts "Please enter a valid coordinate: "
+      end
+
+      # Computer turn
+      @prompts.computer_turn_to_fire_on
+
+      loop do
+        @valid_computer_input = []
+
+        # Select a valid coordinate randomly
+        computer_shot_input = @computer_board.cells.keys.sample(1)
+
+        # The computer should not fire on a space that has already been fired on.
+        unless @user_board.cells[computer_shot_input[0]].fired_upon?
+          @valid_computer_input << computer_shot_input[0]
+          break
+        end
+      end
+
+      @user_board.cells[@valid_computer_input[0]].fire_upon
+
+      # Display results
+      binding.pry
+      puts "Your shot on #{@user_shot_input} was a miss."
+      puts "My shot on #{@valid_computer_input[0]} was a miss."
+
+
+      # When game is over: Break loop when either board has 5 X's
+      if @user_board.render(true).count("X") == 5 || @computer_board.render(true).count("X") == 5
+        # show result
         break
       end
-      puts "Please enter a valid coordinate: "
-    end
 
-    # take turns
+    end
 
   end
 
