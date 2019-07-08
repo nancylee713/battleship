1b77aaa040e38471103a1eb267f1d16ba5d7ff11
Nancy Lee <srlee713@gmail.com>
2019-07-08 16:22:36 -0600

    Create test cases for Player class

--- test/player_test.rb
+++ test/player_test.rb
@@ -10,7 +10,6 @@ class PlayerTest < MiniTest::Test
 
   def setup
     @board = Board.new
-
     @tester_ship_1 = Ship.new("Tester_Ship_1",2)
     @tester_ship_2 = Ship.new("Tester_Ship_2",2)
     @computer = Player.new(@board)
@@ -24,7 +23,14 @@ class PlayerTest < MiniTest::Test
     assert_instance_of Player, @computer
   end
 
-  def test_case_name
+  def test_computer_can_randomly_select_coordinates_to_place_ship
+    computer_choice = @computer.select_random_coordinates(@tester_ship_1)
+    valid_coord_pool = @board.cells.keys
 
+    # test random coordinates belong to the valid pool of 16 coordinates
+    assert_empty computer_choice - valid_coord_pool
+
+    # test the length of a given ship matches # of random coordinates selected
+    assert_equal @tester_ship_1.length, computer_choice.length
   end
 end
