ef1c2ffe9d42bd5dd1ef9a81ae71ad43a21c0fa8
Nancy Lee <srlee713@gmail.com>
2019-07-08 16:22:36 -0600

    Refactor do loops for cruiser and submarine

--- runner.rb
+++ runner.rb
@@ -27,40 +27,38 @@ class PlayBoardGame
     # Computer ship placement & render the board
     @computer_board.place(@cruiser, ["D1", "D2", "D3"])
     puts @computer_board.render
+
+    # Stay in loop until cruiser has valid placement
     @prompts.inquire_cruiser_placement
 
-    # Save user input of 3 coordinates for cruiser & convert to array
-    user_cruiser_input = $stdin.gets.chomp()
-    user_cruiser_placement = user_cruiser_input.split
-
-    # Check whether user input is valid for cruiser
-
-
     loop do
-      if @user_board.valid_placement?(@cruiser, user_cruiser_placement)
-        # Place cruiser and render board
-        @user_board.place(@cruiser, user_cruiser_placement)
+      user_cruiser_input = gets.chomp().split
+      valid_input = @user_board.valid_placement?(@cruiser, user_cruiser_input)
+      if valid_input
+        @user_board.place(@cruiser, user_cruiser_input)
         puts @user_board.render(true)
         break
       end
-
-      puts "Those are invalid coordinates. Please try again:"
-      next
+      puts "Those are invalid coordinates. Please try again: \n>"
     end
 
-    # Save user input of 2 coordinates for submarine
+    # Same for submarine
     @prompts.inquire_submarine_placement
-    user_submarine_input = $stdin.gets.chomp()
-    user_submarine_placement = user_submarine_input.split
 
-    # Check whether user input is valid for submarine
-
-    # Place submarine and render board
-    @user_board.place(@submarine, user_submarine_placement)
-    puts @user_board.render(true)
-    # How to take turns b/w computer and user?
+    loop do
+      user_submarine_input = gets.chomp().split
+      valid_input = @user_board.valid_placement?(@submarine, user_submarine_input)
+      if valid_input
+        @user_board.place(@submarine, user_submarine_input)
+        puts @user_board.render(true)
+        break
+      end
+      puts "Those are invalid coordinates. Please try again: \n>"
+    end
 
   end
+
+  # How to take turns b/w computer and user?
 
 
 end
